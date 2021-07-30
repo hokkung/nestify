d885d9e01cf151cc0f8bd979e325bda2a6a2d33f
hokkung <hokkung.leo@gmail.com>
2021-07-30 17:08:29 +0700

    add module common

--- src/modules/user/user.controller.ts
+++ src/modules/user/user.controller.ts
@@ -1,14 +1,18 @@
 import { Controller, Get } from '@nestjs/common';
-import { UserServiceImpl } from './service/internal/user.service.impl';
+import { UserServiceImpl } from './internal/user.service.impl';
 import { User } from './model/user';
+import { Response } from '../common/model/Response'
 
 @Controller("users")
 export class UserController {
   constructor(private readonly userService: UserServiceImpl) {}
 
   @Get()
-  getUsers(): User[] {
-    return this.userService.getUsers();
+  getUsers(): Response<User> {
+    const response = {
+      data: this.userService.getUsers()
+    }
+    return response;
   }
 
   @Get("/hello")
--- src/modules/user/user.service.ts
+++ src/modules/user/user.service.ts
@@ -1,4 +1,4 @@
-import { User } from '../model/user'
+import { User } from './model/user'
 
 export interface UserService {
   getHello(): string
